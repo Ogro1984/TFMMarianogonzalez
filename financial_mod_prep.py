b17a618b6b8e7f84aa9717263cd1abf0416a0840
marianoVWGroup <mariano.gonzalez@volkswagengroup.es>
2024-05-13 18:52:51 +0200

    more translation

--- financial_mod_prep.py
+++ financial_mod_prep.py
@@ -2,30 +2,30 @@ import requests
 import pandas as pd
 from pymongo import MongoClient
 
-# Define the API key and the tickers
 
+#selecciono los tickers
 tickers = ['NFLX', 'GOOG', 'AMD']
 
 # Connect to the MongoDB server
 client = MongoClient('mongodb+srv://gonzalezracigmariano:hola123@cluster0.qiisexw.mongodb.net/')
 
-# Choose the database
+# Elijo la bbdd
 db = client['financial_data']
 
-# Define the endpoints
+#Defino los endpoints que voy a utilizar desde finalcialmodellingprep
 endpoints = ['quote-short', 'stock-price-change', 'earnings-surprises', 'company-rating', 'key-metrics', 'key-metrics-ttm', 'wma', 'rsi', 'sd', 'historical-price-full']
 
-# Fetch the data for each ticker and endpoint
+# obtengo los datos de cada endpoint para cada ticker
 for ticker in tickers:
     for endpoint in endpoints:
         url = f'https://financialmodelingprep.com/api/v3/{endpoint}/{ticker}'
         response = requests.get(url)
         data = response.json()
 
-        # Choose the collection
+        # elej  la colección
         collection = db[endpoint]
 
-        # Insert the data into the collection
+        # inserto los datos en la colección
         collection.insert_one(data)
 
 
